a4f1295627af427466c0c27933ba92dfcd594b30
Nguyễn Minh <138985110+Ming191@users.noreply.github.com>
2025-12-01 10:25:19 +0700

    Refactor: Use createLogMetadata in requestLoggerMiddleware

--- BE/middlewares/requestLoggerMiddleware.js
+++ BE/middlewares/requestLoggerMiddleware.js
@@ -1,20 +1,30 @@
-import logger from "../logger/winston.log.js";
+import logger, { createLogMetadata } from "../logger/winston.log.js";
 
 export const requestLoggerMiddleware = (req, res, next) => {
     const startTime = Date.now();
 
     // Log request
-    logger.info(`Incoming Request: ${req.method} ${req.originalUrl}`);
+    logger.info(
+        `Incoming Request: ${req.method} ${req.originalUrl}`,
+        createLogMetadata(req, null, null, {}, "API_REQUEST")
+    );
 
     // Log response on finish
     res.on("finish", () => {
-        const duration = Date.now() - startTime;
-        const message = `Response: ${req.method} ${req.originalUrl} ${res.statusCode} ${duration}ms`;
+        const metadata = createLogMetadata(
+            req,
+            res.statusCode,
+            startTime,
+            {},
+            "API_RESPONSE"
+        );
+
+        const message = `Response: ${req.method} ${req.originalUrl} ${res.statusCode} ${metadata.latency}ms`;
 
         if (res.statusCode >= 400) {
-            logger.error(message);
+            logger.error(message, metadata);
         } else {
-            logger.info(message);
+            logger.info(message, metadata);
         }
     });
 
